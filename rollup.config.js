43391f29c66e3c78736af055e62dae30a4310dbd
59naga <i59naga@icloud.com>
2016-04-08 11:52:17 +0900

    fix(build): fix #2
    - replace process.env.NODE_ENV to "proudiction"

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
@@ -13,6 +14,9 @@ export default {
     nodeResolve({ jsnext: true }),
     commonjs(),
     babel(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
     uglify(),
   ],
 };
